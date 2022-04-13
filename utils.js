4975c724acc65c61f652a401acbc85d8e32a154a
Abraham Correa <reianldojunior24@gmail.com>
2022-04-13 09:40:25 -0400

    fix(utils): fix sort by date

--- utils.js
+++ utils.js
@@ -1,3 +1,6 @@
-const orderedByRecent = (arr) => [...arr].sort((item1, item2) => item2 - item1);
+const orderedByRecent = (arr) =>
+  [...arr].sort(
+    (item1, item2) => new Date(item2.created_at) - new Date(item1.created_at)
+  );
 
 module.exports = { orderedByRecent };
